a59830857dfc657a017fed86f4c8417db2cc68ac
Cleberson Primo <cleberson.primo@gmail.com>
2021-06-24 07:48:40 -0300

    Resolução do eercicio

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,20 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+    let id = null;
+    if (typeof user_id !== "string") {
+      [id] = user_id;
+    } else {
+      id = user_id;
+    }
+    try {
+      const users = this.listAllUsersUseCase.execute({ user_id: id });
+
+      return response.status(200).json(users);
+    } catch (e) {
+      return response.status(400).json({ error: e.message });
+    }
   }
 }
 
